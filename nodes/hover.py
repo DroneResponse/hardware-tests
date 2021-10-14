e8202509b8cb1857177b7c5921b13d199389d7d4
Michael Murphy <murphym18@gmail.com>
2021-10-14 11:47:07 -0700

    Clean up the hover test

--- nodes/hover.py
+++ nodes/hover.py
@@ -6,9 +6,13 @@ import dataclasses
 from threading import Condition
 import rospy
 
-from dr_hardware_tests import Drone, FlightMode, SensorSynchronizer, SensorData
+from dr_hardware_tests import Drone, FlightMode, SensorSynchronizer
 from dr_hardware_tests import is_data_available, is_armed, make_func_is_alt_reached, is_loiter_mode
 from dr_hardware_tests import is_disarmed, is_on_ground
+
+
+def log(msg):
+    rospy.loginfo(f"hover test: {msg}")
 
 
 def main():
@@ -17,46 +21,46 @@ def main():
     sensors = SensorSynchronizer()
     sensors.start()
 
-    rospy.loginfo("hover test: waiting for sensor data to come online")
+    log("waiting for sensor data to come online")
     sensors.await_condition(is_data_available, 30)
 
-    rospy.loginfo("hover test: sending arm command")
+    log("sending arm command")
     drone.arm()
 
-    rospy.loginfo("hover test: waiting for drone to arm")
+    log("waiting for drone to arm")
     sensors.await_condition(is_armed, 30)
 
-    rospy.loginfo("hover test: setting takeoff altitude to 7.0 meters")
+    log("setting takeoff altitude to 7.0 meters")
     drone.set_param('MIS_TAKEOFF_ALT', real_value=7.0)
 
     rospy.sleep(1)
-    rospy.loginfo("hover test: sending takeoff command")
+    log("sending takeoff command")
     drone.set_mode(FlightMode.TAKEOFF)
     is_takeoff_alt_reached = make_func_is_alt_reached(7.0)
     sensors.await_condition(is_takeoff_alt_reached, 30)
 
-    rospy.loginfo("hover test: hover in loiter mode")
+    log("hover in loiter mode")
     drone.set_mode(FlightMode.LOITER)
     sensors.await_condition(is_loiter_mode, 5)
 
-    rospy.loginfo("hover test: hover for 30 seconds")
+    log("hover for 30 seconds")
     rospy.sleep(30)
 
-    rospy.loginfo("hover test: send land command")
+    log("send land command")
     drone.set_mode(FlightMode.LAND)
 
-    rospy.loginfo("hover test: waiting to reach the ground")
+    log("waiting to reach the ground")
     sensors.await_condition(is_on_ground)
 
-    rospy.loginfo("hover test: sending disarm command")
+    log("sending disarm command")
     drone.disarm()
-    rospy.loginfo("hover test: waiting for drone to disarm")
+    log("waiting for drone to disarm")
     sensors.await_condition(is_disarmed, 30)
 
-    rospy.loginfo("hover test: SUCCESS")
+    log("SUCCESS")
 
 
 if __name__ == "__main__":
     rospy.init_node("test_hover")
     main()
-    rospy.signal_shutdown("hover test: finished")
+    rospy.signal_shutdown("finished")
