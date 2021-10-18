5057ee1792b8e525aee9c525f3ae6a55d43cd827
Michael Murphy <murphym18@gmail.com>
2021-10-18 13:23:33 -0700

    Fix the sensors test
    We don't want to test the geofence subsystem is reporting data because we have a stand-alone test for that.

--- nodes/sensors.py
+++ nodes/sensors.py
@@ -19,20 +19,21 @@ class _SensorDetectionEvents:
 
 _detected = _SensorDetectionEvents()
 
-_sensor_names = set(map(lambda x: x.name, MAVROS_SENSORS))
+_test_sensors = filter(lambda sensor_meta: sensor_meta.name != 'geofence', MAVROS_SENSORS)
+_sensor_names = set(map(lambda x: x.name, _test_sensors))
 
 
 def test_recv_all_types(data: SensorData) -> bool:
-    a = dataclasses.asdict(data)
+    all_sensor_messages = dataclasses.asdict(data)
+    # Geofence does not need to be checked for this test
+    del all_sensor_messages['geofence']
     missing_messages = set()
-    for name, msg in a.items():
+    for name, msg in all_sensor_messages.items():
         if msg == None:
             missing_messages.add(name)
         else:
             _detected.next_name(name)
 
-    # Geofence does not need to be checked for this test
-    missing_messages.remove("geofence")
     if not missing_messages:
         return True
     else:
