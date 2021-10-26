Fix the quaternion component order
The mavlink protocol expects quaternions in WXYZ order. but the TF library returns quaternions using XYZW order. The gimbal object swaps the order of components so we can pass quaternions from TF library to the set_attitude method.

Add new quaternion values to the gimbal test

nodes/gimbal.py
@@ -16,7 +16,7 @@ from pymavlink import mavutil
 
 from dr_hardware_tests import Drone, SensorSynchronizer, SensorMeta
 from dr_hardware_tests import sleep, start_drone_io
-from tf.transformations import quaternion_from_euler
+from tf.transformations import quaternion_from_euler, quaternion_about_axis, quaternion_multiply
 from pymavlink.dialects.v20 import common as mavlink2
 
 
@@ -24,7 +24,8 @@ from pymavlink.dialects.v20 import common as mavlink2
 def log(msg):
     rospy.loginfo(f"gimbal test: {msg}")
 
-
+def _quaternion_about_axis_d(angle_degrees: float, axis):
+    return quaternion_about_axis(math.radians(angle_degrees), axis)
 
 def main():
     log("initializing")
@@ -34,12 +35,49 @@ def main():
     log("done finding gimbals")
     for gm in gimbal_managers:
         log(f"found gimbal manager: {gm.mavlink_node}")
-    q = quaternion_from_euler(0, 0, math.radians(90))
+    
+    ## 1) turn 90 degrees around the first axis
+    axis = (1, 0, 0)
+    angle = 90
+    q1 = quaternion_about_axis(math.radians(angle), axis)
+
+    ## 2) turn 90 degrees around the second axis
+    angle = 90
+    axis = (0, 1, 0)
+    q2 = quaternion_about_axis(math.radians(angle), axis)
+
+    ## 3) turn 90 degrees around the third axis
+    angle = 90
+    axis = (0, 0, 1)
+    q3 = quaternion_about_axis(math.radians(angle), axis)
+
+    ## 4) look right 90 degrees and look down 45 degrees
+    a = _quaternion_about_axis_d(90, (0, 0, 1))
+    b = _quaternion_about_axis_d(45, (1, 0, 0))
+    # this rotates a then b
+    q4 = quaternion_multiply(b, a)
+
+    ## 5) look left 90 degrees then look up 45 degrees
+    a = _quaternion_about_axis_d(-90, (0, 0, 1))
+    b = _quaternion_about_axis_d(45, (1, 0, 0))
+    q5 = quaternion_multiply(b, a)
+
+    all_quaternions = [q1, q2, q3, q4, q5]
 
     log("taking control of gimbal manager sysid=1, compid=1")
     gimbal_manager = MavlinkNode(1, 1)
     drone.gimbal.take_control(gimbal_manager)
-    drone.gimbal.set_attitude(q, gimbal_manager)
+    
+    log("move the gimbal")
+    for q in all_quaternions:
+        drone.gimbal.set_attitude(q, gimbal_manager)
+        sleep(5)
+        drone.gimbal.reset_attitude(gimbal_manager)
+        sleep(1)
+
+    
+    log("reset gimbal to neutral position")
+    drone.gimbal.reset_attitude(gimbal_manager)
     sleep(1)
     
 

src/dr_hardware_tests/gimbal.py
@@ -10,6 +10,7 @@ from typing import Callable, Dict, List, MutableMapping, NoReturn, Tuple, Union,
 
 import mavros.mavlink
 import rospy
+from tf.transformations import quaternion_about_axis
 
 from mavros_msgs.msg import Mavlink
 from pymavlink import mavutil
@@ -65,7 +66,7 @@ class MavlinkNode:
     system_id: SystemId
     component_id: ComponentId
 
-
+# quaternions are represented x, y, z, w (to match what the tf library does)
 Quaternion = NewType('Quaternion', Tuple[float, float, float, float])
 
 
@@ -163,6 +164,7 @@ class _CmdType(enum.Enum):
     GET_GIMBALS = enum.auto()
     RECV_MAVLINK = enum.auto()
     SET_ATTITUDE  = enum.auto()
+    RESET_GIMBAL = enum.auto()
     TAKE_CONTROL = enum.auto()
 
 
@@ -206,6 +208,7 @@ class Gimbal:
             _CmdType.RECV_MAVLINK: self._recv_mavlink,
             _CmdType.SET_ATTITUDE: self._set_attitude,
             _CmdType.TAKE_CONTROL: self._take_control,
+            _CmdType.RESET_GIMBAL: self._reset_gimbal,
         }
 
         self._recv_mavlink_method = {
@@ -284,11 +287,33 @@ class Gimbal:
         self.cmd_queue.put(cmd)
     
     def set_attitude(self, q: Quaternion, gimbal_manager: Union[MavlinkNode, GimbalManager], gimbal_id: ComponentId = 0):
+        """Set the attitude of the gimbal. 
+        
+        The quaternion parameter is specified using the convention from TF (X, Y, Z, W).
+        The quaternion applies rotation in a forward right down coordinate system.
+
+        The gimbal manager and gimbal device specify which gimbal you want to control. you need to
+        take control of the gimbal manager before using this method or else the gimbal manager will
+        ignore the command. You can use the `take_control` method to set this object as a controller.
+        """
+        # MAVLink expects w, x, y, z order but our Quaternion follows the x, y, z, w convention from the tf lib
+        q = q[3], *q[:3]
         if type(gimbal_manager) == GimbalManager:
             gimbal_manager = gimbal_manager.mavlink_node
         cmd = _Command(
             command_type=_CmdType.SET_ATTITUDE,
             quaternion=q,
+            gimbal_manager=gimbal_manager,
+            gimbal_device_id = gimbal_id,
+        )
+        self.cmd_queue.put(cmd)
+    
+    def reset_attitude(self, gimbal_manager: Union[MavlinkNode, GimbalManager], gimbal_id: ComponentId = 0):
+        if type(gimbal_manager) == GimbalManager:
+            gimbal_manager = gimbal_manager.mavlink_node
+        cmd = _Command(
+            command_type=_CmdType.RESET_GIMBAL,
+            gimbal_device_id = gimbal_id,
             gimbal_manager=gimbal_manager,
         )
         self.cmd_queue.put(cmd)
@@ -429,6 +454,19 @@ class Gimbal:
             flags=_GimbalManagerFlags.GIMBAL_MANAGER_FLAGS_NOTHING.value,
             gimbal_device_id=msg.gimbal_device_id,
             q=msg.quaternion,
+            angular_velocity_x=math.nan,
+            angular_velocity_y=math.nan,
+            angular_velocity_z=math.nan,
+        )
+        self.mav_sender.send(mav_cmd)
+    
+    def _reset_gimbal(self, cmd: _Command):
+        mav_cmd = mavlink2.MAVLink_gimbal_manager_set_attitude_message(
+            target_system=cmd.gimbal_manager.system_id,
+            target_component=cmd.gimbal_manager.component_id,
+            flags=_GimbalManagerFlags.GIMBAL_MANAGER_FLAGS_NEUTRAL.value,
+            gimbal_device_id=cmd.gimbal_device_id,
+            q=[math.nan, math.nan, math.nan, math.nan],
             angular_velocity_x=math.nan,
             angular_velocity_y=math.nan,
             angular_velocity_z=math.nan,