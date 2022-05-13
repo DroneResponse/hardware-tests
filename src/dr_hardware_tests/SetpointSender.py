bf72208fadadb3ff7f54835a887e634cf6940c61
Michael Murphy <murphym18@gmail.com>
2022-05-12 21:50:33 -0700

    SetpointSender can stream velocity setpoints
    The RC fail safe is too aggressive. It causes the program to exit immediately after arming. To fix the problem we want the user to enter offboard mode after arming. Since we need to stream setpoints before we can enter offboard mode, we want to stream velocity = 0 setpoints.  That's why our setpoint sender needs the ability to send velocity. Once we're in offboard mode we can enable the RC fail safe. 
    See GitHub Issue #12

--- src/dr_hardware_tests/SetpointSender.py
+++ src/dr_hardware_tests/SetpointSender.py
@@ -4,6 +4,7 @@ import enum
 from dataclasses import dataclass
 from queue import Queue
 from threading import Thread, Event
+from typing import Tuple
 from genpy import message
 
 import rospy
@@ -15,6 +16,8 @@ from .Drone import Drone
 class _Flag(enum.Enum):
     GET_SETPOINT = enum.auto()
     SET_SETPOINT = enum.auto()
+    GET_VELOCITY = enum.auto()
+    SET_VELOCITY = enum.auto()
     SEND_NOW = enum.auto()
     STOP = enum.auto()
 
@@ -22,6 +25,7 @@ class _Flag(enum.Enum):
 class _Message:
     flag: _Flag
     setpoint: Position = None
+    velocity: Tuple[float, float, float] = None
     data_channel: Queue = None
     done_event: Event = None
 
@@ -32,6 +36,8 @@ class SetpointSender:
         self._drone: Drone = drone
         self._message_queue:Queue = Queue()
         self._setpoint: Lla = None
+        self._velocity: Tuple[float, float, float] = None
+        self._mode = None
         self._sender_thread = Thread(target=self._run)
 
         self._timer_thread = Thread(target=self._run_timer)
@@ -66,6 +72,20 @@ class SetpointSender:
         message = _Message(flag=_Flag.SET_SETPOINT, setpoint=lla, done_event=done_event)
         self._message_queue.put(message)
         done_event.wait()
+    
+    @property
+    def velocity(self) -> Tuple[float, float, float]:
+        result_channel = Queue()
+        message = _Message(flag=_Flag.GET_VELOCITY, data_channel=result_channel)
+        self._message_queue.put(message)
+        return result_channel.get(timeout=1.0)
+    
+    @velocity.setter
+    def velocity(self, ned: Tuple[float, float, float]):
+        done_event = Event()
+        message = _Message(flag=_Flag.SET_VELOCITY, velocity=ned, done_event=done_event)
+        self._message_queue.put(message)
+        done_event.wait()
 
     def _run(self):
         def stop_callback():
@@ -80,10 +100,11 @@ class SetpointSender:
                 return
 
             elif message.flag == _Flag.SET_SETPOINT:
-                new_setpoint =  message.setpoint
+                new_setpoint = message.setpoint
                 if new_setpoint is not None:
                     new_setpoint = new_setpoint.to_lla()
                 self._setpoint = new_setpoint
+                self._velocity = None
                 
             elif message.flag == _Flag.GET_SETPOINT:
                 getter_return_value = None
@@ -92,10 +113,23 @@ class SetpointSender:
                     lat, lon, alt = self._setpoint.lat, self._setpoint.lon, self._setpoint.alt
                     getter_return_value = Lla(lat, lon, alt)
                 message.data_channel.put(getter_return_value)
-
+            
+            elif message.flag == _Flag.SET_VELOCITY:
+                self._velocity = message.velocity
+                self._setpoint = None
+            
+            elif message.flag == _Flag.GET_VELOCITY:
+                getter_return_value = None
+                if self._velocity is not None:
+                    # copy it
+                    getter_return_value = tuple(self._velocity)
+                message.data_channel.put(getter_return_value)
+                
             elif message.flag == _Flag.SEND_NOW:
                 if self._setpoint is not None:
                     self._drone.send_setpoint(self._setpoint)
+                elif self._velocity is not None:
+                    self._drone.send_velocity(self._velocity)
             
             if message.done_event is not None:
                 message.done_event.set()
