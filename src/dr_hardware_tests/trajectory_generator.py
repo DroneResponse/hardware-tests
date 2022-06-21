eaa736fa838863bea9c1f023edc60843539597f0
Michael Murphy <murphym18@gmail.com>
2022-06-21 10:56:04 -0700

    WIP trajectory generator #16
    Add a unit test for the trajectory function.

--- src/dr_hardware_tests/trajectory_generator.py
+++ src/dr_hardware_tests/trajectory_generator.py
@@ -1,15 +1,46 @@
+import enum
 import time
-
-from concurrent.futures import thread
+from typing import Callable
+from dataclasses import dataclass
+from queue import Queue
 from threading import Event, Thread
-from droneresponse_mathtools import Lla, Pvector
-from dr_hardware_tests.SetpointSender import SetpointSender
+
+import rospy
+from droneresponse_mathtools import Lla, geoid_height
 from mavros_msgs.srv import ParamGet
-from ruckig import InputParameter, Ruckig, Trajectory, Result
+from ruckig import InputParameter, Result, Ruckig, Trajectory
 
-from nodes.box import ellipsoid_to_amsl
+from dr_hardware_tests.Drone import Drone
+from dr_hardware_tests.SetpointSender import SetpointSender
 
-from .Drone import Drone
+
+def amsl_to_ellipsoid(pos: Lla) -> Lla:
+    """
+    Return the Lla with its altitude specified as meters above the ellipsoid 
+    Given an Lla with it's altitude specified as meters above mean sea level (AMSL)
+    """
+    amsl_alt = pos.altitude
+    geoid_separation = geoid_height(pos.latitude, pos.longitude)
+    ellipsoid_alt = amsl_alt + geoid_separation
+    return Lla(pos.latitude, pos.longitude, ellipsoid_alt)
+
+
+def ellipsoid_to_amsl(pos: Lla) -> Lla:
+    """
+    Return the Lla with its altitude specified as meters above mean sea level
+    Given an Lla with it's altitude specified as meters above the ellipsoid
+    """
+    wgs84_alt = pos.altitude
+    geoid_separation = geoid_height(pos.latitude, pos.longitude)
+    amsl_alt = wgs84_alt - geoid_separation
+    return Lla(pos.latitude, pos.longitude, amsl_alt)
+
+
+# TrajectoryFunction returns the drone's position along the path as a function of time.
+# Where the time is given as seconds since the maneuver started.
+# The position returned has its altitude specified as meters above mean sea level
+# By convention, instances of this function are often called "s"
+TrajectoryFunction = Callable[[float], Lla]
 
 
 class TrajectoryGenerator:
@@ -34,11 +65,22 @@ class TrajectoryGenerator:
         self.max_down_acceleration = abs(max_down_acceleration)
         self.max_jerk = abs(max_jerk)
     
-    def make(self, start_pos: Lla, stop_lla: Lla, speed: float):
+    def make(self, start_pos: Lla, stop_pos: Lla, speed: float) -> TrajectoryFunction:
         """
-        Make a function s(t)
+        Make a TrajectoryFunction, s(t).
 
-        It returns the setpoint LLA given time since we started the maneuver in seconds
+        The TrajectoryFunction returns the drone's position along the path as a function of time.
+        Where the time is given as seconds since the maneuver started. The position returned
+        specifies its altitude in meters above mean sea level
+
+        Arguments:
+            start_pos: Lla of the drone's position when the maneuver starts with
+                it's altitude specified as meters above the ellipsoid
+            stop_pos: Lla of the destination with altitude given as meters above
+                the ellipsoid
+            speed: how fast the drone should fly in meters per second
+
+        
         """
         inp = InputParameter(3)
 
@@ -46,7 +88,7 @@ class TrajectoryGenerator:
         inp.current_velocity = [0.0, 0.0, 0.0]
         inp.current_acceleration = [0.0, 0.0, 0.0]
 
-        stop_ned = start_pos.distance_ned(stop_lla)
+        stop_ned = start_pos.distance_ned(stop_pos)
         stop_north = stop_ned[0]
         stop_east = stop_ned[1]
         stop_down = stop_ned[2]
@@ -66,7 +108,7 @@ class TrajectoryGenerator:
             self.max_horizontal_acceleration,
             self.max_down_acceleration
         ]
-        inp.min_velocity = [
+        inp.min_acceleration = [
             -1.0 * self.max_horizontal_acceleration,
             -1.0 * self.max_horizontal_acceleration,
             -1.0 * self.max_up_acceleration
@@ -95,10 +137,12 @@ class TrajectoryGenerator:
             east = new_position[1]
             down = new_position[2]
 
-            target_lla = start_pos.move_ned(north, east, down)
-            return target_lla
+            target_lla_ellipsoid = start_pos.move_ned(north, east, down)
+            target_lla_amsl = ellipsoid_to_amsl(target_lla_ellipsoid)
+            return target_lla_amsl
         
         return s, trajectory.duration
+
 
 def make_TrajectoryGenerator(drone: Drone):
     def read_param(param_name):
@@ -114,6 +158,67 @@ def make_TrajectoryGenerator(drone: Drone):
 
     return TrajectoryGenerator(max_horizontal_acceleration, max_up_acceleration, max_down_acceleration, max_jerk)
 
+
+class _Message(enum.Enum):
+    SEND_NOW = enum.auto()
+    STOP = enum.auto()
+
+
+class TrajectorySender:
+    def __init__(self, s: TrajectoryFunction, duration: float, setpoint_sender: SetpointSender,  send_frequency: float = 20):
+        self._s = s
+        self._duration = duration
+        self._setpoint_sender = setpoint_sender
+        self._send_frequency = send_frequency
+        self._message_queue: Queue = Queue()
+        self._stop_event = Event()
+
+        self._update_thread = Thread(target=self._run)
+        self._timer_thread = Thread(target=self._run_timer)
+
+    def start(self):
+        self._update_thread.start()
+        self._timer_thread.start()
+
+    def stop(self, await_stop=True):
+        self._stop_event.set() # stops the timer
+        if not self._update_thread.is_alive():
+            return
+        self._message_queue.put(_Message.STOP)
+        if await_stop:
+            self._update_thread.join()
+
+
+    def _run(self):
+        def stop_callback():
+            self.stop(await_stop=False)
+
+        rospy.on_shutdown(stop_callback)
+
+        start_time = time.time()
+        t = 0
+        self._setpoint_sender.setpoint = self._s(t)
+        while not rospy.is_shutdown() and t < self._duration:
+            message: _Message = self._message_queue.get()
+            if message.message_type == _Message.STOP:
+                return
+            elif message == _Message.SEND_NOW:
+                t = time.time() - start_time
+                self._setpoint_sender.setpoint = self._s(t)
+            else:
+                rospy.logerr(f"Unknown message type {message}.")
+    
+    def _run_timer(self):
+        message = _Message(message_type=_Message.SEND_NOW)
+        rate = rospy.Rate(self._send_frequency)
+        while not rospy.is_shutdown() and not self._stop_event.is_set():
+            try:
+                if self._update_thread.is_alive():
+                    self._message_queue.put(message)
+                rate.sleep()
+            except rospy.ROSInterruptException:
+                pass
+    
 def fly_trajectory(s, duration: float, setpoint_sender: SetpointSender):
     result = Event()
     def fly():
@@ -122,8 +227,7 @@ def fly_trajectory(s, duration: float, setpoint_sender: SetpointSender):
 
         t = 0
         while start_time + t < end_time:
-            lla = s(t)
-            setpoint_sender.lla = ellipsoid_to_amsl(lla)
+            setpoint_sender.lla = s(t)
             time.sleep(0.05)
             t = time.time() - start_time
         result.set()            
